a1bcd90a2c7ad3a542f734a76a4a3fe80ae51d27
nicolasances <nicolas.matteazzi@gmail.com>
2020-06-21 16:41:50 +0200

    initial import

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require("body-parser");
+var fs = require('fs');
 
 app = express();
 
@@ -16,11 +17,34 @@ app.use(bodyParser.json());
 app.get('/status', (req, res) => {
 
     res.status(200).type('application/json').send({
+        name: 'kapi-vol',
         status: 'running',
         msg: 'All is fine man! I am up and running'
     })
 });
 
+app.post('/data', (req, res) => {
+
+    let home = process.env['HOME'];
+    let folderName = 'my-data';
+
+    let dir = home + '/' + folderName;
+
+    if (!fs.existsSync(dir)) {
+        console.log("[kapi-vol] - Creating " + dir);
+        fs.mkdirSync(dir);
+    }
+
+    fs.writeFile(dir + '/helloworld.txt', 'Hello World!', function (err) {
+        if (err) {
+            res.status(500).type('application/json').send({ written: false, error: err })
+            return;
+        }
+
+        res.status(200).type('application/json').send({ written: true })
+    });
+});
+
 app.listen(8080, () => {
-    console.log('[kapi-base] - Up and running');
+    console.log('[kapi-vol] - Up and running');
 });
